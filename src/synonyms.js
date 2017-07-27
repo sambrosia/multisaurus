fe0dae96c9c8ffc1159602a5362bc8304c772e41
sambrosia <samuelcwoodruff@gmail.com>
2017-07-27 13:35:42 -0600

    Return more synonyms
    Modifies synonyms() to return a big list of synonyms for every word.
    This behaviour will change to pull from multiple APIs and rank
    synonyms in order to get the best results.

--- src/synonyms.js
+++ src/synonyms.js
@@ -1,6 +1,23 @@
 import synonyms from 'synonyms'
 
-export default async function (words) {
-  if (words.length < 1) return
-  return synonyms(words[0])
+export default (words) => {
+  if (words.length < 1) return []
+  const allSynonyms = []
+
+  for (let word in words) {
+    word = words[word]
+    const raw = synonyms(word) || {}
+
+    for (const type in raw) {
+      for (let synonym in raw[type]) {
+        synonym = raw[type][synonym]
+
+        if (synonym.length > 1 && allSynonyms.indexOf(synonym) < 0) {
+          allSynonyms.push(synonym)
+        }
+      }
+    }
+  }
+
+  return allSynonyms
 }
