6a2b350988d18b22b0d7886a8fc383ea889000f7
sambrosia <samuelcwoodruff@gmail.com>
2017-07-27 12:06:55 -0600

    Add mutations for adding and removing words

--- src/store.js
+++ src/store.js
@@ -3,7 +3,16 @@ export default {
     words: []
   },
 
-  mutations: {},
+  mutations: {
+    addWord (state, word) {
+      if (word !== '' && !state.words.includes(word)) state.words.push(word)
+    },
+    removeWord (state, word) {
+      const index = state.words.indexOf(word)
+      if (index < 0) return
+      state.words.splice(index, 1)
+    }
+  },
 
   actions: {}
 }
