9f56c6fd003a8303e6e1d681f7f00529c620b06f
sambrosia <samuelcwoodruff@gmail.com>
2017-08-01 20:00:30 -0600

    Cache thesaurus lookups for each word

--- src/synonyms.js
+++ src/synonyms.js
@@ -1,12 +1,19 @@
 import synonyms from 'synonyms'
 
+const cache = {
+  local: {}
+}
+
 export default (words) => {
   if (words.length < 1) return []
   const allSynonyms = []
 
   for (let word in words) {
     word = words[word]
-    const raw = synonyms(word) || {}
+    const raw = cache.local[word] || synonyms(word) || {}
+
+    // Cache the results for this thesaurus/word ('local' is the thesaurus)
+    cache.local[word] = raw
 
     for (const type in raw) {
       for (let synonym in raw[type]) {
