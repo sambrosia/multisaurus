5f1a40f416eeb6960076ace3ea6941dcec645ed2
sambrosia <samuelcwoodruff@gmail.com>
2017-08-04 18:54:50 -0600

    Fix synonym ranking
    All synonyms were ranked 0 due to a poorly written if condition.

--- src/synonyms.js
+++ src/synonyms.js
@@ -8,19 +8,18 @@ export default (words) => {
   if (words.length < 1) return []
   const allSynonyms = {}
 
-  for (let word in words) {
-    word = words[word]
+  for (const word of words) {
     const raw = cache.local[word] || synonyms(word) || {}
 
     // Cache the results for this thesaurus/word ('local' is the thesaurus)
     cache.local[word] = raw
 
     for (const type in raw) {
-      for (let synonym in raw[type]) {
-        synonym = raw[type][synonym]
+      for (const syn in raw[type]) {
+        const synonym = raw[type][syn]
 
         if (synonym.length > 1) {
-          if (allSynonyms[synonym]) allSynonyms[synonym]++
+          if (allSynonyms[synonym] !== undefined) allSynonyms[synonym]++
           else allSynonyms[synonym] = 0
         }
       }
